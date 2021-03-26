6121b154ef2c0429bc51f973815f2573aa323ccd
WreetSarker <wreet.sarker@gmail.com>
2021-03-27 00:23:38 +0600

    first commit

---
+++ ema-john-simple/src/components/Review/Review.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import fakeData from '../../fakeData';
+import { getDatabaseCart, processOrder, removeFromDatabaseCart } from '../../utilities/databaseManager';
+import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
+import './Review.css';
+import happyFace from '../../images/giphy.gif';
+import { useHistory } from 'react-router';
+
+const Review = () => {
+    const [cart, setCart] = useState([]);
+    const [orderPlaced, setOrderPlaced] = useState(false);
+
+    const history = useHistory();
+    const handleProceedCheckout = () => {
+        history.push('/shipment');
+    }
+    const handleRemoveProduct = (productKey) => {
+        const newCart = cart.filter(pd => pd.key !== productKey);
+        setCart(newCart);
+        removeFromDatabaseCart(productKey);
+    }
+    useEffect(() => {
+        const savedCart = getDatabaseCart();
+        const productKeys = Object.keys(savedCart);
+        const cartProducts = productKeys.map(key => {
+            const product = fakeData.find(pd => pd.key === key);
+            product.quantity = savedCart[key];
+            return product;
+        });
+        console.log(cartProducts);
+        setCart(cartProducts);
+    }, [])
+    let thanksImage;
+    if (orderPlaced) {
+        thanksImage = <img src={happyFace} alt="" />
+    }
+    return (
+        <div className="twin-container">
+            <div className="product-container">
+                {
+                    cart.map(pd => <ReviewItem key={pd.key} handleRemoveProduct={handleRemoveProduct} product={pd}></ReviewItem>)
+                }
+                {
+                    thanksImage
+                }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}>
+                    <button onClick={handleProceedCheckout} className="main-button">Proceed Checkout</button>
+                </Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Review;
---
+++ ema-john-simple/src/components/Shop/Shop.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import fakeData from '../../fakeData';
+import { addToDatabaseCart, getDatabaseCart } from '../../utilities/databaseManager';
+import Cart from '../Cart/Cart';
+import Product from '../Product/Product';
+import './Shop.css';
+import { Link } from 'react-router-dom';
+
+const Shop = () => {
+    const firstTen = fakeData.slice(0, 10);
+    const [products] = useState(firstTen);
+    const [cart, setCart] = useState([]);
+
+    useEffect(() => {
+        const savedCart = getDatabaseCart();
+        const productKeys = Object.keys(savedCart);
+        const cartProducts = productKeys.map(key => {
+            const product = fakeData.find(pd => pd.key === key);
+            product.quantity = savedCart[key];
+            return product;
+        });
+        // console.log(cartProducts);
+        setCart(cartProducts);
+    }, [])
+    const handleAddProduct = (product) => {
+        const toBeAdded = product.key;
+        const sameProduct = cart.find(pd => pd.key === toBeAdded);
+        let count = 1;
+        let newCart;
+        if (sameProduct) {
+            count = sameProduct.quantity + 1;
+            sameProduct.quantity = count;
+            const others = cart.filter(pd => pd.key !== toBeAdded);
+            newCart = [...others, sameProduct];
+        } else {
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }
+        setCart(newCart);
+        addToDatabaseCart(product.key, count);
+    }
+    return (
+        <div className="twin-container">
+            <div className="product-container">
+                {
+                    products.map(product => <Product key={product.key} showCartBtn={true} handleAddProduct={handleAddProduct} product={product}></Product>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}>
+                    <Link to="/review"><button className="main-button">Review Order</button></Link>
+                </Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
